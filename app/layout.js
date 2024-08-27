589923aa330652dd0249b4fe9be07f13ca0d8c66
Parker Mathis <parkerjmathis@gmail.com>
2024-08-26 18:33:29 -0700

    Update assets

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 const spaceGrotesk = Space_Grotesk({
@@ -16,7 +17,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="max-w-10xl mx-auto">{children}</main>
+        <main className="max-w-10xl mx-auto">
+          <Navbar />
+          {children}
+        </main>
       </body>
     </html>
   );
